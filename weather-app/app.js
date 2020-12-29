7269b06e2471416267e9a67154d6b7bde28a8b09
Nandan Raj <nandanraj56@gmail.com>
2020-12-29 23:30:38 +0530

    lec 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=12992ffc526f209ee4207732da9fdb7c&query=37.8267,-122.42';
 
-request({url:url},(error, response)=>{
-    const data = JSON.parse(response.body);
+request({url:url, json:true},(error, response)=>{
+    const data = response.body.current;
     //console.log(data)
-    console.log(data.current)
+    //console.log(response.body.current)
+    console.log(data.weather_descriptions[0]+'. it is currently '+data.temperature+' degress out. It feels like '+data.feelslike+' degress out!');
 });
