f6e816cf747a39c39d9b2b76480178e6cb517412
Nandan Raj <nandanraj56@gmail.com>
2020-12-31 11:57:55 +0530

    lec 37

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,40 +1,15 @@
 const request = require('request');
-const url = 'http://api.weatherstack.com/current?access_key=12992ffc526f209ee4207732da9fdb7c&query=37.8267,-122.42';
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-/*request({url:url, json:true},(error, response)=>{
+
+geocode('New Jersy',(error,response)=>{
+    console.log(error);
+    console.log(response);
     
-    if(error){
-        console.log('Unable to connect to weather service!');
-        
-    }else if(response.body.error){
-        console.log('Unbale to find location!');
-
-    }
-    else{
-        const data = response.body.current;
-        console.log(data.weather_descriptions[0]+'. it is currently '+data.temperature+' degress out. It feels like '+data.feelslike+' degress out!');
-    }
-    //console.log(data)
-    //console.log(response.body.current)
-    
-});*/
-
-const mapurl = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmFuZGFucmFqNTYiLCJhIjoiY2tqYWF2aWM2MjcxajJzbnF2bXU0c2ZmaCJ9.0Co3EsHOltSdKXEFaY4Nsg&limit=1';
-
-request({url:mapurl,json:true},(error,response)=>{
-    if(error){
-        console.log('Unable to connect to location service');
-    }else if(response.body.features.length <=0){
-        console.log('Invalid location');
-    }else{
-        const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitude, longitude);
-    }
-
-    //console.log(response.body.features[0].center);
-    
-
-
-
 });
+
+forecast(-75.7088, 44.145, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  });
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const forecast = (latitude,longitude, callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=12992ffc526f209ee4207732da9fdb7c&query='+longitude+','+ latitude;
+    request({url:url, json:true},(error, response)=>{
+    
+        if(error){
+            callback('Unable to connect to weather service!',undefined);
+            
+        }else if(response.body.error){
+            callback('Unable to find location!',undefined);
+    
+        }
+        else{
+            const data = response.body.current;
+            callback(undefined,data.weather_descriptions[0]+'. it is currently '+data.temperature+' degress out. It feels like '+data.feelslike+' degress out!');
+        }
+      
+        
+    });
+
+};
+
+module.exports = forecast;
