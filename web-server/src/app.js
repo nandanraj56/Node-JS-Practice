62ed79377fd7aa8315246eba39a9c0dfd4739808
Nandan Raj <nandanraj56@gmail.com>
2021-01-09 15:03:10 +0530

    Lec 54 55: Json endpoint, Query paramater

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express()
 
@@ -40,11 +42,44 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
+    if(!req.query.address){
+        return res.send({
+            Error:'You must specify address.'
+        });
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location})=>{
+        if(error){
+            return res.send({
+                error,
+            })
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address:req.query.address
+            })
+
+        });
+
+        /*res.send({
+            forecast: 'It is snowing',
+            location: 'Philadelphia',
+            address:req.query.address
+        })*/
+    });
+   
 })
+
+app.get('/products',(req, res)=>{
+    console.log(req.query)
+    res.send(req.query);
+});
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
