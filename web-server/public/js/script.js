a8331615d408cb7eb86ce854331bc20f21e7fe38
Nandan Raj <nandanraj56@gmail.com>
2021-01-10 22:23:15 +0530

    lec 59: sec 8 Accessing api from browser complete

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -7,6 +7,8 @@ const weatherForm = document.querySelector('form');
 console.log(weatherForm)
 weatherForm.addEventListener('submit',(event)=>{
     event.preventDefault();
+    document.querySelector('#pOne').textContent = "Loading...";
+    document.querySelector('#pTwo').textContent = "";
     console.log('Gaya yaha');
     const inputAddress = document.getElementsByName('address')[0].value;
     console.log(inputAddress)
@@ -16,11 +18,16 @@ weatherForm.addEventListener('submit',(event)=>{
         //console.log(data);
         if(data.error){
             console.log(data.error);
+            document.querySelector('#pOne').textContent = data.error;
+            document.querySelector('#pTwo').textContent = "";
         }
         else{
-            console.log(data.forecast)
-            console.log(data.location)
-            console.log(data.address)
+            document.querySelector('#pOne').textContent = data.forecast;
+            document.querySelector('#pTwo').textContent = data.location;
+            
+            //console.log(data.forecast)
+            //console.log(data.location)
+            //console.log(data.address)
         }
     })
 
