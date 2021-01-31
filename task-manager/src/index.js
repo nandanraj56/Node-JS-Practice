f1f50de393e88aa9b33e33c1961a4f4a8a3fc387
Nandan Raj <nandanraj56@gmail.com>
2021-01-31 16:09:35 +0530

    lec 97:IIntegrating async await into task api

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 require("./db/mongoose")
 const User = require("./models/user")
-const Task= require("./models/task")
+const Task = require("./models/task")
 const app = express()
 
 
@@ -11,69 +11,77 @@ const ObjectID = mongodb.ObjectID;
 app.use(express.json())
 
 const port = process.env.PORT || 3000
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Listening')
 })
 
-app.post('/users',(req,res)=>{
-    
-    const user = new User(req.body)
-    user.save().then((data)=>{
+app.post('/users', async (req, res) => {
+    try {
+        const user = new User(req.body)
+        const data = await user.save()
         res.status(201).send(data)
-    }).catch((error)=>{
+    }
+    catch (error) {
         res.status(400).send(error)
-    })
+    }
 
-    //res.send("Hello")
 })
 
-app.get("/users",(req,res)=>{
-    User.find({}).then((users)=>{
-       /* if(users.length == 0)
-            return res.status(404).send()*/
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({})
+        if (users.length == 0)
+            return res.status(404).send()
         res.send(users)
-    }).catch((e)=>{
+    } catch (error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get("/users/:id",(req,res)=>{
-    const _id = req.params.id
-    User.findById(_id).then((user)=>{
-        if(!user)
+app.get("/users/:id",async (req, res) => {
+    try {
+        const _id = req.params.id
+        const user = await User.findById(_id)
+        if (!user) {
             return res.status(404).send()
+        }
         res.send(user)
-    }).catch((e)=>{
+
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
-app.post("/tasks",(req,res)=>{
-    const task = new Task(req.body)
-
-    task.save().then((data)=>{
-        res.status(201).send(data)
-    }).catch((e)=>{
+app.post("/tasks",async (req, res) => {
+    
+    try{
+        const task = new Task(req.body)
+        await task.save()
+        res.status(201).send(task)
+    }
+    catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get("/tasks",(req,res)=>{
-    Task.find({}).then((data)=>{
-        res.send(data)
-    }).catch((e)=>{
+app.get("/tasks",async (req, res) => {
+    try{
+        const task = await Task.find({})
+        res.send(task)
+    }catch(e){
         res.status(500).send(e)
-    })
+    }
 })
 
-app.get("/tasks/:id",(req,res)=>{
-    console.log(req.params.id)
-    const _id = req.params.id//new ObjectID(req.params.id)
-    Task.findById(_id).then((data)=>{
-        if(!data)
+app.get("/tasks/:id",async (req, res) => {
+    try{
+        const _id = req.params.id//new ObjectID(req.params.id)
+        const task = await Task.findById(_id)
+        if (!task)
             return res.status(404).send()
-        res.send(data)
-    }).catch((e)=>{
+        res.send(task)
+    }catch(e){
         res.status(500).send()
-    })
+    }
+    
 })
