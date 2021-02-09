9fff93c0687932bb756ea6c8376223d594df8672
Nandan Raj <nandanraj56@gmail.com>
2021-02-09 12:13:22 +0530

    114 2, user/task relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,9 +21,14 @@ app.listen(port, () => {
 })
 
 const main = async() => {
+    //task-> user
+    /*const task = await Task.findById("id of task")
+    await task.populate("owner").execPopulate()
+    console.log(task.owner)
+    //user-> tasks
     const user = await User.findById('602172562036403500229b1e')
     await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
+    console.log(user.tasks)*/
 
 }
 main()
