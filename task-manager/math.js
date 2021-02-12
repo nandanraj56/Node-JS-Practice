49989941ea6b78dac31d8e6f95a9816f109a4788
Nandan Raj <nandanraj56@gmail.com>
2021-02-12 14:38:50 +0530

    140 Testing async code

--- task-manager/math.js
+++ task-manager/math.js
@@ -8,9 +8,17 @@ const fahrenheitToCelsius = (temp) => {
 const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
+const add = (a,b) =>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },2000)
+    })
+}
 
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,5 +1,5 @@
 const {calculateTip} = require("../math")
-const {celsiusToFahrenheit,fahrenheitToCelsius} = require("../math")
+const {celsiusToFahrenheit,fahrenheitToCelsius,add} = require("../math")
 test('hello',()=>{
 
 })
@@ -18,4 +18,18 @@ test('Should convert 32 F to 0 C',()=>{
 })
 test('Should convert 0 C to 32 F',()=>{
     expect(celsiusToFahrenheit(0)).toBe(32)
+})
+//one, way to call async code
+test('add two numbers test',(done)=>{
+    add(3,4).then((data)=>{
+        expect(data).toBe(7)
+        done()
+    }
+    )
+})
+
+//Better way to test async code
+test('add two number test async',async()=>{
+    const sum = await add(5,8)
+    expect(sum).toBe(13)
 })
